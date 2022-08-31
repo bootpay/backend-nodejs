47c6de7185e75b95a8be9e83b7b75d9e6b9d243a
gosomi <gosomi@udid.co.kr>
2022-08-31 09:31:18 +0900

    changelog 업데이트

--- test/requestCashReceipt.js
+++ test/requestCashReceipt.js
@@ -1,21 +1,21 @@
 (async () => {
     const Bootpay = require('../dist/bootpay.js').Bootpay
-    // Bootpay.setConfiguration({
-    //     application_id: '5b8f6a4d396fa665fdc2b5ea',
-    //     private_key: 'rm6EYECr6aroQVG2ntW0A6LpWnkTgP4uQ3H18sDDUYw='
-    // })
     Bootpay.setConfiguration({
-        application_id: '59bfc738e13f337dbd6ca48a',
-        private_key: 'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
-        mode: 'development'
+        application_id: '59b731f084382614ebf72215',
+        private_key: 'WwDv0UjfwFa04wYG0LJZZv1xwraQnlhnHE375n52X0U='
     })
+    // Bootpay.setConfiguration({
+    //     application_id: '59bfc738e13f337dbd6ca48a',
+    //     private_key: 'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+    //     mode: 'development'
+    // })
     try {
         // console.log(new Date((new Date()).getTime() + 5000))
         await Bootpay.getAccessToken()
         const response = await Bootpay.requestCashReceipt({
-            pg: '토스',
+            pg: '나이스페이',
             price: 1000,
-            tax_free: 0,
+            tax_free: 500,
             order_name: '테스트',
             cash_receipt_type: '소득공제',
             user: {
@@ -25,7 +25,6 @@
             },
             identity_no: '0100000000',
             order_id: (new Date()).getTime(),
-            reserve_execute_at: new Date((new Date()).getTime())
         })
         console.log(response)
         if (response.receipt_id !== undefined) {
