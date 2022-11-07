08aef1e65f4da63081eb3da29de12adf5657f1bd
gosomi <gosomi@udid.co.kr>
2022-11-07 13:52:29 +0900

    resource version 업데이트

--- src/lib/resource.ts
+++ src/lib/resource.ts
@@ -24,8 +24,8 @@ export class BootpayBackendNodejsResource {
     mode: string
     bootpayConfiguration: BootpayConfiguration
     API_ENTRYPOINTS: BootpayEntrypoints
-    apiVersion: string = '4.2.2'
-    sdkVersion: string = '2.1.2'
+    apiVersion: string = '4.2.5'
+    sdkVersion: string = '2.1.5'
 
     constructor() {
         this.mode = 'production'
